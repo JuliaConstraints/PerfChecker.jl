4d6a9d3ae5c6bcea196a9cef62b9d774570d72c7
azzaare <jf@baffier.fr>
2023-02-01 12:56:23 +0900

    Fix for Julia 1.8 and 1.9. New version. Update CI

--- test/pattern_folds.jl
+++ test/pattern_folds.jl
@@ -21,8 +21,6 @@ using PatternFolds
         collect(i)
         reverse(collect(i))
 
-        rand(i, 1000)
-
         # Vectors
         vf = make_vector_fold([0, 1], 2, 1000)
         @info "Checking VectorFold" vf pattern(vf) gap(vf) folds(vf) length(vf)
@@ -38,4 +36,36 @@ using PatternFolds
 
     # Actual call to PerfChecker
     alloc_check(title, dependencies, targets, alloc, alloc; path=@__DIR__)
+
 end
+
+target = PatternFolds
+
+function bench() # 0.2.x
+    # Intervals
+    itv = Interval{Open,Closed}(0.0, 1.0)
+    i = IntervalsFold(itv, 2.0, 1000)
+
+    unfold(i)
+    collect(i)
+    reverse(collect(i))
+
+    # rand(i, 1000)
+
+    # Vectors
+    vf = make_vector_fold([0, 1], 2, 1000)
+    # @info "Checking VectorFold" vf pattern(vf) gap(vf) folds(vf) length(vf)
+
+    unfold(vf)
+    collect(vf)
+    reverse(collect(vf))
+
+    rand(vf, 1000)
+
+    return nothing
+end
+
+t = @benchmark bench() evals = 1 samples = 1000 seconds = 3600
+
+# Actual call to PerfChecker
+store_benchmark(t, target; path=@__DIR__)
