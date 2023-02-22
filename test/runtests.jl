47529a288602f1981556815d2ce4181620734e7b
Azzaare <jf@baffier.fr>
2023-02-22 11:07:32 +0900

    Fix compat. Add a compat check in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,10 @@ using BenchmarkTools
 using PerfChecker
 using Test
 
+import CompatHelperLocal
+
+CompatHelperLocal.@check()
+
 # include("compositional_networks.jl")
 
 include("pattern_folds.jl")
