b8052eaeb2f3bd08db0ca7e61e894fba0686758b
Chetan Vardhan <51269425+VarLad@users.noreply.github.com>
2023-03-19 17:33:49 +0530

    Update init.jl
    "file://" is not needed now because Term uses it as of the latest version :)

--- src/init.jl
+++ src/init.jl
@@ -2,7 +2,7 @@ function __init__()
     if isinteractive()
         eval(Meta.parse("""
         function TypedTables.showtable(io::IO, t::TypedTables.Table{NamedTuple{(:bytes, :percentage, :filenames, :linenumbers), Tuple{Int64, Float64, String, Int64}}, 1, NamedTuple{(:bytes, :percentage, :filenames, :linenumbers), Tuple{Vector{Int64}, Vector{Float64}, Vector{String}, Vector{Int64}}}})
-            fn = Term.Link.("file://" .* t.filenames, t.linenumbers)
+            fn = Term.Link.(t.filenames, t.linenumbers)
             Term.tprint(io, Term.Table([t.bytes t.percentage fn]; header = ["bytes", "ratio (%)", "filenames (links)"]))
         end
         """))
