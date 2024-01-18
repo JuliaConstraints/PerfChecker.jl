559eb6be68cca82370818f332d7cd20bb0e8db51
Azzaare <jf@baffier.fr>
2024-01-18 08:18:40 +0000

    The great cleansing

--- src/PerfChecker.jl
+++ src/PerfChecker.jl
@@ -1,35 +1,20 @@
 module PerfChecker
 
 # SECTION - Imports
-using CSV
-using DataFrames
-using LibGit2
-using OrderedCollections
-using PGFPlotsX
 using Pkg
-using Plots
 using Profile
-using Random
-using StatsPlots
-using Term
+
 import TypedTables: Table, showtable
 import Distributed: remotecall_fetch, addprocs, rmprocs
 import CoverageTools: analyze_malloc_files, find_malloc_files, MallocInfo
 
 # SECTION - Exports
-export alloc_check
-export alloc_plot
-# export bench_plot
-# export store_benchmark
 export @check
 export to_table
 
 # SECTION - Includes
 
-include("init.jl")
 include("check.jl")
 include("alloc.jl")
-# include("benchmarks.jl")
-include("utils.jl")
 
 end
