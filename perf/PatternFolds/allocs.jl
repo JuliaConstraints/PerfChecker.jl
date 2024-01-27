4103703fb5cf06ec161700dbf2f1da00aa94d520
Azzaare <jf@baffier.fr>
2024-01-27 15:19:18 +0100

    Clean old files and restore PatternFolds allocations script in perf

---
+++ perf/PatternFolds/allocs.jl
@@ -0,0 +1,24 @@
+using PerfChecker
+
+@check :alloc Dict(:target => ["PatternFolds"], :path => @__DIR__) begin
+    using PatternFolds
+    end begin
+    itv = Interval{Open,Closed}(0.0, 1.0)
+    i = IntervalsFold(itv, 2.0, 1000)
+
+    @info "Checking IntervalsFold" i pattern(i) gap(i) folds(i) size(i) length(i)
+
+    unfold(i)
+    collect(i)
+    reverse(collect(i))
+
+    # Vectors
+    vf = make_vector_fold([0, 1], 2, 1000)
+    @info "Checking VectorFold" vf pattern(vf) gap(vf) folds(vf) length(vf)
+
+    unfold(vf)
+    collect(vf)
+    reverse(collect(vf))
+
+    rand(vf, 1000)
+end
--- src/PerfChecker.jl
+++ src/PerfChecker.jl
@@ -1,20 +1,13 @@
 module PerfChecker
 
 # SECTION - Imports
-#using DataFrames
-using LibGit2
-using OrderedCollections
 using Pkg
 using Profile
-using Random
 import TypedTables: Table
 import Distributed: remotecall_fetch, addprocs, rmprocs
 import CoverageTools: analyze_malloc_files, find_malloc_files, MallocInfo
 
 # SECTION - Exports
-export alloc_check
-export alloc_plot
-export store_benchmark
 export @check
 export to_table
 
@@ -22,7 +15,5 @@ export to_table
 
 include("check.jl")
 include("alloc.jl")
-#include("benchmarks.jl")
-#include("utils.jl")
 
 end
