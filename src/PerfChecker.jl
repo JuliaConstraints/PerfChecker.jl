6ff242bb3225fc5f4cddaa69668456e6be1ffabc
Chetan Vardhan <chetan.verdhan8@gmail.com>
2024-03-06 09:51:04 +0530

    Initial changes

--- src/PerfChecker.jl
+++ src/PerfChecker.jl
@@ -6,6 +6,44 @@ using Profile
 import TypedTables: Table
 import Distributed: remotecall_fetch, addprocs, rmprocs
 import CoverageTools: analyze_malloc_files, find_malloc_files, MallocInfo
+import Sys
+import CpuId
+
+struct HwInfo
+    cpus::Vector{CPUInfo}
+    machine::String
+    word::Int
+    simdbytes::Int
+    corecount::Tuple{Int, Int}
+end
+
+struct CheckerResult
+    table::Table
+    hwinfo::HwInfo
+    tags::Vector{Symbol}
+end
+
+function find_by_tags(tags::Vector{Symbol}, results::CheckerResult; exact_match = true)
+    results = []
+    if exact_match
+        for j in results
+            if tags == j.tags
+                push!(results, tags)
+            else
+        end
+    else
+        for j in results
+            for i in tags
+                if i in j.tags
+                    push!(results, tags)
+                    break
+                end
+            end
+        end
+    end
+        
+    return results
+end
 
 # SECTION - Exports
 export @check
--- src/check.jl
+++ src/check.jl
@@ -12,18 +12,52 @@ function default_options(d::Dict, v::Symbol)
   return merge(di, d)
 end
 
+function resolve_versions(d::Dict{String,Any})
+    for key,value in d
+        d[key] = resolve_version(x)
+    end
+end
+
+function resolve_version(x::String, y::VersionNumber)
+    
+end
+
+function resolve_version(x::String, y::String)
+
+end
+
+function resolve_version(x::String, y::Symbol)
+
+end
+
 macro check(x, d, block1, block2)
     block1, block2 = Expr(:quote, block1), Expr(:quote, block2)
     quote
         di = default_options($d, $x)
         g = prep(di, $block1, $x)
         h = check(di, $block2, $x)
+        import Pkg
+        Pkg.instantiate()
         p = remotecall_fetch(Core.eval, 1, Main,
             Expr(:toplevel, quote
                 import Distributed
                 d = $di
+                if typeof(d[:path]) == Dict{String, Any}
+                    d[:path] == mktemp()[1]
+                end
                 Distributed.addprocs(1; exeflags=["--track-allocation=$(d[:track])", "--project=$(d[:path])", "-t $(d[:threads])"])
             end).args...) |> first
+
+        remotecall_fetch(Core.eval, p, Main,
+            Expr(:toplevel, quote
+                import Pkg;
+                Pkg.instantiate();
+            ).args...)
+
+            
+        if typeof(di.path) == Dict{String, Any}
+            
+        end
 
         di[:prep_result] = remotecall_fetch(Core.eval, p, Main,
             Expr(:toplevel, g.args...))
