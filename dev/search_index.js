af878efff64b44df99b3ac57b6a1b19aa4b52398
Documenter.jl <documenter@juliadocs.github.io>
2024-05-29 06:26:47 +0000

    build based on 7f3eba7

--- dev/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PerfChecker","category":"page"},{"location":"#PerfChecker","page":"Home","title":"PerfChecker","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PerfChecker.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PerfChecker]","category":"page"}]
-}
