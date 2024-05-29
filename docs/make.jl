47bf5be309918675346b4f61318bd1c9bf8a3792
Azzaare <jf@baffier.fr>
2024-05-29 03:36:44 +0000

    Move doc to DocumenterVitepress

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,24 @@
-using PerfChecker
-using Documenter
+using Documenter, DocumenterVitepress
 
-DocMeta.setdocmeta!(PerfChecker, :DocTestSetup, :(using PerfChecker); recursive = true)
+using PerfChecker
 
 makedocs(;
     modules = [PerfChecker],
-    authors = "Azzaare <jf@baffier.fr>",
-    repo = "https://github.com/JuliaConstraints/PerfChecker.jl/blob/{commit}{path}#{line}",
+    authors = "azzaare <jf@baffier.fr>",
+    repo = "https://github.com/JuliaConstraints/PerfChecker.jl",
     sitename = "PerfChecker.jl",
-    format = Documenter.HTML(;
-        prettyurls = get(ENV, "CI", "false") == "true",
-        canonical = "https://JuliaConstraints.github.io/PerfChecker.jl",
-        assets = String[]
+    format = DocumenterVitepress.MarkdownVitepress(
+        repo = "https://github.com/JuliaConstraints/PerfChecker.jl",
+        devurl = "dev",
+        deploy_url = "JuliaConstraints.github.io/PerfChecker.jl"
     ),
     pages = [
         "Home" => "index.md"
-    ]
+    ],
+    warnonly = true
 )
 
 deploydocs(;
     repo = "github.com/JuliaConstraints/PerfChecker.jl",
-    devbranch = "main"
+    push_preview = true
 )
